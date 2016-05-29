5b4a467d42d13b01a587402352491a8a89d7b947
Carolyn <carolyn.lee@yale.edu>
2016-05-29 16:01:13 -0400

    Add static files

---
+++ static/js/hotels.js
@@ -0,0 +1,13 @@
+;(function($){
+    // Equalize the panel heights
+    var panels = $("#hotels .panel");
+    $(window).on("load resize", function(evt){
+        equalizeHeights(panels);
+    });
+    function equalizeHeights(collection) {
+        var maxHeight = collection.map(function(i, elem) {
+            return $(elem).height();
+        }).get();
+        return collection.css("min-height", Math.max.apply(null, maxHeight));
+    }
+}(jQuery));
