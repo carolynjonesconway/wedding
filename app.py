0596deb1659b8c1f9ae3de47c53dcdfeac673864
Carolyn <carolyn.lee@yale.edu>
2016-09-14 11:23:43 -0700

    Add error pages

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from helpers import url_for
 def build_app(name):
     static_folder = "./static"
     app = Flask(name, static_folder=static_folder, static_url_path="/static")
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.jinja_env.globals['url_for'] = url_for
     os.environ.get('SECRET_KEY', str(uuid1()))
     return app
--- server.py
+++ server.py
@@ -3,18 +3,35 @@ from flask import render_template
 from app import build_app
 
 
-app = build_app(__name__)
 DEBUG = 'PROD' not in os.environ
 PORT = os.environ.get('PORT', 5000)
+
+app = build_app(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = DEBUG
 
 
 @app.route("/")
 def home():
-    return render_template("main.html")
+	return render_template("main.html")
+
+@app.route("/error/<int:code>")
+def error(code):
+	if code == 500:
+		return render_template('500.html'), 500
+	return render_template('404.html'), 404
 
 @app.route("/es")
 def home_es():
     return render_template("es/main.html")
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def internal_server_error(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
-    app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
+	app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
+
